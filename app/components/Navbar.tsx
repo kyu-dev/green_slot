da2e77f4eae4fe5184249a25d77d2f20a151d521
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-04-08 18:40:32 +0200

    inprove animation

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LetterSwapForward from './LetterSwapForward';
 
 const Navbar = () => {
   return (
@@ -6,10 +7,13 @@ const Navbar = () => {
       <h1 className="text-white text-2xl font-semibold">Green Slot</h1>
       <nav className="rounded-full bg-white/20 backdrop-blur-xs w-fit border-white/30 border py-4 px-8 ">
         <ul className="flex text-white gap-4 font-medium">
-          <li>Home</li>
-          <li>Program</li>
-          <li>Contact</li>
-          <li>About</li>
+          <LetterSwapForward label="First" staggerFrom={'first'} />
+          <LetterSwapForward
+            label="Center"
+            staggerFrom={'center'}
+            className=""
+          />
+          <LetterSwapForward label="Last" staggerFrom={'last'} />
         </ul>
       </nav>
       <button className="rounded-full bg-white py-4 px-6 font-normal">
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ArrowRight } from 'lucide-react';
+import VerticalCutReveal from './components/VerticalCutReveal';
 
 const page = () => {
   return (
@@ -7,8 +8,20 @@ const page = () => {
       <div className="flex justify-between items-center h-full w-full px-20 relative">
         <div className="flex flex-col text-left max-w-xl ">
           <h2 className="text-8xl font-bold mb-4 text-white ">
-            SUSTAINABLE LIVING SIMPLIFIED
+            <VerticalCutReveal
+              splitBy="characters"
+              staggerDuration={0.065}
+              staggerFrom="first"
+              transition={{
+                type: 'spring',
+                stiffness: 200,
+                damping: 21,
+              }}
+            >
+              {`SUSTAINABLE LIVING SIMPLIFIED`}
+            </VerticalCutReveal>
           </h2>
+
           <p className="text-lg text-white">
             Foundation® merges expert insight with cutting-edge technology to
             redefine investment property financing – making it smarter, faster,
@@ -18,7 +31,7 @@ const page = () => {
         <img
           src="/image.png"
           alt="Illustration de vie durable"
-          className="w-180 h-auto ml-8"
+          className="w-180 h-auto ml-8 hover:scale-110 transition-transform duration-300"
         />
         <button className="absolute bottom-0 left-0 z-10 bg-[#FB9434] px-8 py-3 text-white flex gap-6">
           Apply Now
